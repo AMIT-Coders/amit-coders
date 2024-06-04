fe36f52705750a9e5a5a3e01dcb3d4154085eb4e
Mohamed Ibrahim Hany <60690890+mihany@users.noreply.github.com>
2024-06-04 09:55:29 +0300

    more code added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import heroHomepage from "./components/HeroHomepage/HeroHomepage";
-import Faqs from "./components/Faqs/Faqs";
+import Faqs from "./components/FAQs/FAQs";
 import HeroHomepage from "./components/HeroHomepage/HeroHomepage";
 import Hero2Homepage from "./components/Hero2Homepage/Hero2Homepage";
 import Hero3Homepage from "./components/Hero3Homepage/Hero3Homepage";
@@ -9,6 +9,7 @@ import HomepageBlogs from "./components/HomepageBlogs/HomepageBlogs";
 import Trustedby from "./components/Trustedby/trustedby";
 import Definitions from "./components/Definitions/Definitions";
 import Empoweryourskills from "./components/Empoweryourskills/Empoweryourskills";
+import FAQs from "./components/FAQs/FAQs";
 export default function Home() {
   return (
     <>
@@ -20,7 +21,7 @@ export default function Home() {
       <HomepageBlogs />
       <Hero3Homepage />
       <Trustedby />
-      <Faqs />
+      <FAQs />
     </>
   );
 }
